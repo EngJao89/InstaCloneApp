7a6de0d908493c32622a2dd73f317ed679a0861e
EngJao89 <eng_joao@outlook.com>
2025-01-28 23:18:55 -0300

    create service of recover posts

--- InstaCloneApp/TableViewCell/PostagemTableViewCell.swift
+++ InstaCloneApp/TableViewCell/PostagemTableViewCell.swift
@@ -25,3 +25,4 @@ class PostagemTableViewCell: UITableViewCell {
     }
 
 }
+
--- InstaCloneApp/viewController/abas/HomeViewController.swift
+++ InstaCloneApp/viewController/abas/HomeViewController.swift
@@ -6,17 +6,57 @@
 //
 
 import UIKit
+import FirebaseFirestore
+import FirebaseAuth
+import FirebaseUI
 
 class HomeViewController: UIViewController,
     UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var tableViewPostagens: UITableView!
-    
+    var firestore: Firestore!
+    var auth: Auth!
+    var postagens: [Dictionary<String, Any>] = []
+    var idUsuarioLogado: String!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableViewPostagens.separatorStyle = .none
+        
+        firestore = Firestore.firestore()
+        auth = Auth.auth()
+        
+        if let idUsuario = auth.currentUser?.uid {
+            self.idUsuarioLogado = idUsuario
+        }
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        recuperarPostagens()
+    }
+    
+    func recuperarPostagens(){
+        
+        //Limpa listagem de postagens
+        self.postagens.removeAll()
+        self.tableViewPostagens.reloadData()
+        
+        firestore.collection("postagens")
+        .document(idUsuarioLogado)
+        .collection("postagens_usuario")
+            .getDocuments { (snapshotResultado, erro) in
+                
+                if let snapshot = snapshotResultado {
+                    for document in snapshot.documents {
+                        let dados = document.data()
+                        self.postagens.append(dados)
+                    }
+                    self.tableViewPostagens.reloadData()
+                }
+                
+        }
         
     }
     
@@ -26,15 +66,23 @@ class HomeViewController: UIViewController,
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return self.postagens.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let celula = tableView.dequeueReusableCell(withIdentifier: "celulaPostagens", for: indexPath) as! PostagemTableViewCell
         
-        celula.descricaoPostagem.text = "Descrição..."
-        celula.fotoPostagem.image = UIImage(named: "padrao")
+        let indice = indexPath.row
+        let postagem = self.postagens[indice]
+        
+        let descricao = postagem["descricao"] as? String
+        if let url = postagem["url"] as? String {
+            celula.fotoPostagem.sd_setImage(with: URL(string: url), completed: nil)
+        }
+        
+        celula.descricaoPostagem.text = descricao
+        //celula.fotoPostagem.image = UIImage(named: "padrao")
         
         return celula
         
